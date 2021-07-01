50bc6057ad7eb89a44bd474eba8b1ef9f14cfc80
xiaobao <yinxiaobao@aliyun.com>
2021-07-01 10:06:18 +0800

    update fonts

--- modules/fonts.nix
+++ modules/fonts.nix
@@ -2,21 +2,8 @@
 
 {
   fonts = {
-    enableDefaultFonts = true;
-    fontconfig = {
-      enable = true;
-      defaultFonts = {
-        emoji = [ "Noto Color Emoji" ];
-        serif = [ "Vazir" "Ubuntu" ];
-        sansSerif = [ "Vazir" "Ubuntu" ];
-        monospace = [ "Ubuntu" ];
-      };
-    };
-    fontDir.enable = true;
-    enableGhostscriptFonts = true;
     fonts = with pkgs; [
       ubuntu_font_family
-      vazir-fonts
     ];
   };
 }
