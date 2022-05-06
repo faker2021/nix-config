023d24d993a81176edc41de653dbde548a0df888
xiaobao <yinxiaobao@aliyun.com>
2022-05-06 09:53:05 +0800

    update wst.nix

--- home/wst.nix
+++ home/wst.nix
@@ -1,4 +1,26 @@
-{ config, pkgs, lib, ... }:
-{
+{ config, pkgs, lib, ... }: {
+  home.packages = [
+    pkgs.unzip
+    pkgs.wget
+    pkgs.gcc
+    pkgs.gdb
+    pkgs.cmake
+    pkgs.python38
+
+  ];
+
+  programs.home-manager.enable = true;
+
+  programs.git = {
+    package = pkgs.gitAndTools.gitFull;
+    enable = true;
+    userName = "faker";
+    userEmail = "nspyia2002@gmail.com";
+    signing = {
+      key = "nspyia2002@gmail.com";
+      signByDefault = false;
+    };
+  };
+
 
 }
