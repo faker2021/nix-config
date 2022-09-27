78fa79ad41768bb479a11c22c860530ecdf6c468
faker2021 <nspyia2002@gmail.com>
2022-09-27 16:46:03 +0800

    add programs.git

--- home/user-yxb.nix
+++ home/user-yxb.nix
@@ -8,6 +8,18 @@
     pkgs.nixpkgs-fmt
   ];
 
+  programs.git = {
+    package = pkgs.gitAndTools.gitFull;
+    enable = true;
+    userName = "faker";
+    userEmail = "nspyia2002@gmail.com";
+    signing = {
+      key = "nspyia2002@gmail.com";
+      signByDefault = false;
+    };
+  };
+
+
   home.stateVersion = "22.05";
   programs.home-manager.enable = true;
 }
